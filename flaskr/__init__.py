523e4b3704b5137595d5022d60109dc2e544f79a
Nishshanka Sirisena <nishshanka123@gmail.com>
2024-05-30 14:22:33 +0100

    enhance the base template and add DB config, connectivity

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,39 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Flask application factory create_app()
+def create_app(app_config=None):
+    # create and configure the app
+    # create a flask instance
+    app = Flask(__name__, instance_relative_config=True)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    # SECRET_KEY is used by Flask and extensions to keep data safe. 
+    # It’s set to 'dev' to provide a convenient value during development, but it should be 
+    # overridden with a random value when deploying.
+    app.config.from_mapping(
+        SECRET_KEY='dev',        
+    )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # implement loading the application configuration from an external file
+    if app_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(app_config)
+
+    # route to website index
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        return render_template('index.html')
+
+    #initialize the database connectivity
+    
+    return app
+
+    app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8080')
--- flaskr/db.py
+++ flaskr/db.py
@@ -1,1 +1,26 @@
 # Database access 
+import mysql.connector
+from flask import g
+
+HOST = 'localhost'
+USER = 'nishshanka'
+PASSWORD = 'malsara'
+DATABASE = 'db_esystem'
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host=HOST,
+            user=USER,
+            password=PASSWORD,
+            database=DATABASE,
+            autocommit=True
+        )
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
