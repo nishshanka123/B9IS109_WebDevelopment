6cdad1df0272669d031872a357e23d398070d209
Nishshanka Sirisena <nishshanka123@gmail.com>
2024-06-15 20:19:01 +0100

    Register user and blueprint

--- flaskr/authenticate.py
+++ flaskr/authenticate.py
@@ -13,7 +13,67 @@ authenticate_bp = Blueprint('authenticate', __name__, url_prefix='/auth')
 
 @authenticate_bp.route('/registerUser', methods=['GET', 'POST'])
 def registerUser():
-    print("registerUser: ------------> 1")
+    user_name = None
+    email = None
+    password = None
+    password_c = None
+    security_q = None
+    security_qa = None    
+
+    confirm_password = None
+    country = None
+    terms_accepted = False
+    error = None
+    if request.method == 'POST':
+        user_name = request.form['user_name']
+        email = request.form['email']
+        password = request.form['password']
+        password_c = request.form['confirm_password']
+        security_q = request.form['sec_qlist']
+        security_qa = request.form ['sec_qa']
+        terms_accepted = 'terms' in request.form
+
+        if not user_name:
+            error = 'User name should not empty'
+        elif not password:
+            error = 'Password should not empty'
+        
+        print(f"Auth: {terms_accepted}")
+        if not terms_accepted:
+            error = 'Terms and Conditions are not accepted.'
+
+
+        if error is None:
+            #get the db from current application
+            db = current_app.config['db']
+            if db is None:
+                print("DB connection is None")
+
+            try:
+                insert_q = '''INSERT INTO auth_info (user_name, email, pwd_hash, security_question_id, sq_answer, role_id) values (%s, %s, %s, %s, %s, %s)'''
+                #print(f"insert_q",(user_name, email, password, security_q, security_qa, 1) )
+                #insert_q = "INSERT INTO user_info (email, first_name, last_name, user_name, date_of_birth, address_line1, address_line2, area_code) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                #db.execute_query("INSERT INTO auth_info (user_name, email, pwd_hash, security_question_id, sq_answer, role_id) values (%s, %s, %s, %s, %s, %s)", (user_name, email, password, security_q, security_qa, '1') )
+                db.execute_query(insert_q, (user_name, email, password, security_q, security_qa, '1') )
+
+            except Exception as ex:
+                #flash(f"Error: {str(ex)}", 'danger')
+                print(f"Database error occurred: {ex}")
+            else:
+                return redirect(url_for("authenticate.login"))
+        else:
+            print(f"Form submission error: {error}")
+            flash(error, 'danger')
+            return redirect(url_for('authenticate.registerUser') )
+    elif request.method == 'GET':
+        pass
+
+
+    return render_template('auth/registerUser.html')
+
+@authenticate_bp.route('/updateUser', methods=['GET', 'POST'])
+def updateUser():
+    print("updateUser: ------------> 1")
     print("request.form: " , request.form)
     user_name = None
     email = None
@@ -29,7 +89,7 @@ def registerUser():
     country = None
     tnc = False
     if request.method == 'POST':
-        print("registerUser: ------------> 2")
+        print("updateUser: ------------> 2")
         name = request.form['name']
         email = request.form['email']
         first_name = request.form['first_name']
@@ -57,6 +117,8 @@ def registerUser():
             db.execute_query(insert_q, email, first_name, last_name, user_name, date_of_birth, address_line1, address_line2, area_code)
         except Exception as ex:
             flash(f"Error: {str(ex)}", 'danger')
+        else:
+            return redirect(url_for("auth.login"))
             
 
     return render_template('auth/registerUser.html')
